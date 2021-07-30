aab7b2c627cf1ccc6d5ff7113b8c823c336f3627
Mira <sma88000@gmail.com>
2021-07-31 03:01:12 +0500

    add VK token and function get_params(TOKEN)

---
+++ main.py
@@ -0,0 +1,7 @@
+TOKEN = '35cbc798d0e2fc93d9e9fb388b93ea44d42e26fb1e2b6ec8cd1788cabb17c52eba8d01f4f4ee608edcffa'
+
+def get_params(TOKEN): # обязательные параметры
+    return {
+    'access_token': TOKEN,
+    'v': 5.89,
+    }
