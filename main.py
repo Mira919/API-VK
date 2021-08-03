5950dfb3ef1209af08a9807012533656f76591ca
Mira <sma88000@gmail.com>
2021-08-03 09:16:02 +0500

    fix bags

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import json
+import requests
+import pprint
+import time
+
 TOKEN = '35cbc798d0e2fc93d9e9fb388b93ea44d42e26fb1e2b6ec8cd1788cabb17c52eba8d01f4f4ee608edcffa'
 
 def get_params(TOKEN): # обязательные параметры
     return {
     'access_token': TOKEN,
-    'v': 5.89,
+    'v': 5.131,
     }
 
 def get_groupINFO_user(): # получаем информацию о группах пользователя
